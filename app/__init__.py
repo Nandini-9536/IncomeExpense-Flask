0250c5c09b9c17f38bb8cc5a75712f3427ddc4e5
Nandini-9536 <nandinisisodiya1002@gmail.com>
2025-02-06 13:23:41 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+
 
 app = Flask(__name__)
 app.app_context().push()
 
 app.config['SECRET_KEY'] = "JLKJJJO3IURYoiouolnojojouuoo=5y9y9youjuy952oohhbafdnoglhoho"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenseDB.db'
-db_path= os.path.join('/tmp','sqlite:///expenseDB.db')
+
 
 db = SQLAlchemy(app)
 
